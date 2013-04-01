b88572a31a9424295a82cbaee4d99165d39267aa
Les Orchard <me@lmorchard.com>
2013-04-01 16:52:24 -0400

    Implement max_jobs_per_process option for Pool

--- test/basic-test.js
+++ test/basic-test.js
@@ -1,4 +1,6 @@
 // TODO: Test new pool events
+// TODO: Better reporting of uncaughtExceptions - just get an "exit" right now,
+//       need message & stack
 // TODO: Test backlog vs hireling pool
 // TODO: Optionally enforce a max backlog size with immediate job errors
 // TODO: Enforce max job execution time
@@ -12,6 +14,8 @@ var util = require('util'),
     hirelings = require('../lib/hirelings');
 
 var CONCURRENCY = 4;
+var MAX_JOBS = 4;
+var LOTS_OF_JOBS = ((CONCURRENCY * MAX_JOBS) + 10);
 
 var suite = vows.describe('Basic hirelings tests');
 
@@ -110,6 +114,50 @@ suite.addBatch({
                             ['failure', 'THIS IS A FAILURE']);
                     }
                 }
+            }
+        }
+    }
+});
+
+suite.addBatch({
+    'a Pool with max_job_per_process=4': {
+        topic: function () {
+            var self = this;
+            var processes = {};
+            var pool = new hirelings.Pool({
+                max_jobs_per_process: MAX_JOBS,
+                concurrency: CONCURRENCY,
+                module: __dirname + '/workers/echo.js',
+                options: { thing: 'ohai' }
+            });
+            // Watch for spawning processes
+            pool.on('spawn', function (p) {
+                processes[p.process.pid] = 0;
+            });
+            // Count jobs tasked to processes
+            pool.on('task', function (job, p) {
+                processes[p.process.pid]++;
+            });
+            // Queue up an arbitrarily large number of jobs.
+            var jobs = [];
+            for (var i=0; i < LOTS_OF_JOBS; i++) {
+                var job = pool.enqueue({something: "interesting"});
+                jobs.push(job);
+            }
+            // When the Pool has drained, we're done with the topic.
+            pool.on('drain', function () {
+                self.callback(null, processes, pool, jobs);
+            });
+            // Make sure we have an exit point for the pool.
+            setTimeout(function () { pool.exit(); }, 10000);
+        },
+        'should result in no processes having performed more than 4 jobs': 
+                function (err, processes, pool, jobs) {
+            for (pid in processes) {
+                var num_jobs = processes[pid];
+                assert.ok(num_jobs <= MAX_JOBS,
+                    "Process " + pid + " should have performed " + MAX_JOBS +
+                    " or less, but performed " + num_jobs);
             }
         }
     }
